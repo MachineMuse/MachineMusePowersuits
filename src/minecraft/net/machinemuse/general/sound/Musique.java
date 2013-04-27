bf6c120e6de0ce05a16c85ad9028b0840c49f40c
MachineMuse <muse@machinemuse.net>
2013-04-27 02:32:22 -0600

    made hud elements only render when no gui is open

--- src/minecraft/net/machinemuse/general/sound/Musique.java
+++ src/minecraft/net/machinemuse/general/sound/Musique.java
@@ -1,17 +1,15 @@
 package net.machinemuse.general.sound;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 
 public class Musique {
-	public static void playOneshotSound(EntityPlayer player, String soundname, float volume) {
-		if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
-			Minecraft.getMinecraft().sndManager.playSoundFX(SoundLoader.SOUND_JUMP_ASSIST,
-					1.0f, 1.0f);
-			// Minecraft.getMinecraft().sndManager.playEntitySound(SoundLoader.SOUND_JUMP_ASSIST,
-			// player, 1.0f, 1.0f, true);
-		}
-	}
+    public static void playOneshotSound(EntityPlayer player, String soundname, float volume) {
+        if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
+            Minecraft.getMinecraft().sndManager.playSoundFX(SoundLoader.SOUND_JUMP_ASSIST, 1.0f, 1.0f);
+            // Minecraft.getMinecraft().sndManager.playEntitySound(SoundLoader.SOUND_JUMP_ASSIST, player, 1.0f, 1.0f, true);
+        }
+    }
 }
