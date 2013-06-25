b00b051bd913a74ae16b96aac37f959da9e6f9fb
MachineMuse <claire.alexandria@gmail.com>
2013-06-25 09:12:05 -0600

    Added RenderPlayerAPI's ModelPlayer to src tree...hope this works

---
+++ src/minecraft/net/minecraft/src/ModelPlayer.java
@@ -0,0 +1,13 @@
+package net.minecraft.src;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class ModelPlayer extends net.minecraft.client.model.ModelBiped
+{
+    public ModelPlayer(float var1)
+    {
+        super(var1);
+    }
+}
