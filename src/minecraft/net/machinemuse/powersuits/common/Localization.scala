d0309af45f0a8492b69e0f4b2238c743a327f393
MachineMuse <claire.alexandria@gmail.com>
2013-06-29 00:52:21 -0600

    Localization tidying

--- src/minecraft/net/machinemuse/powersuits/common/Localization.scala
+++ src/minecraft/net/machinemuse/powersuits/common/Localization.scala
@@ -1,6 +1,6 @@
 package net.machinemuse.powersuits.common
 
-import net.minecraft.util.StringTranslate
+import net.minecraft.util.{StatCollector, StringTranslate}
 import java.io._
 import net.machinemuse.general.MuseLogger
 import java.util.Properties
@@ -17,17 +17,24 @@ object Localization {
   def getCurrentLanguage = StringTranslate.getInstance().getCurrentLanguage
 
   def loadCurrentLanguage() {
-    val lang = getCurrentLanguage
-    try {
-      val inputStream: InputStream = this.getClass.getResourceAsStream(LANG_PATH + lang + ".lang")
-      val langPack: Properties = new Properties
-      langPack.load(inputStream)
-      LanguageRegistry.instance.addStringLocalization(langPack, lang)
-    } catch {
-      case e: Exception => {
-        e.printStackTrace()
-        MuseLogger.logError("Couldn't read MPS localizations for language " + lang + " :(")
+    if (getCurrentLanguage != extractedLanguage) {
+      extractedLanguage = getCurrentLanguage
+      try {
+        val inputStream: InputStream = this.getClass.getResourceAsStream(LANG_PATH + extractedLanguage + ".lang")
+        val langPack: Properties = new Properties
+        langPack.load(inputStream)
+        LanguageRegistry.instance.addStringLocalization(langPack, extractedLanguage)
+      } catch {
+        case e: Exception => {
+          e.printStackTrace()
+          MuseLogger.logError("Couldn't read MPS localizations for language " + extractedLanguage + " :(")
+        }
       }
     }
+  }
+
+  def translate(str: String) = {
+    loadCurrentLanguage()
+    StatCollector.translateToLocal(str)
   }
 }
