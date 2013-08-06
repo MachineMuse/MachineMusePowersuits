5b2f8b42606eb4bcb27e31087d8f3f9ae6bd0064
MachineMuse <claire.alexandria@gmail.com>
2013-08-06 17:09:17 -0600

    more case sensitivity irritation

--- src/minecraft/net/machinemuse/powersuits/common/Config.java
+++ src/minecraft/net/machinemuse/powersuits/common/Config.java
@@ -43,8 +43,8 @@ import java.util.Properties;
  * @author MachineMuse
  */
 public class Config {
-    public static final String RESOURCE_PREFIX = "/assets/mmmpowersuits/";
-    public static final String TEXTURE_PREFIX = "mmmpowersuits:textures/";
+    public static final String RESOURCE_PREFIX = "/assets/powersuits/";
+    public static final String TEXTURE_PREFIX = "powersuits:textures/";
     public static final String SOUND_PREFIX = RESOURCE_PREFIX + "sound/";
     public static final String LANG_PREFIX = RESOURCE_PREFIX + "lang/";
     // public static final String SEBK_ICON_PATH =
