773159b396d1fa136d9de50ca5ba06f3263f9d08
MachineMuse <claire.alexandria@gmail.com>
2013-08-07 11:07:51 -0600

    change sprint assist to use new attribute system

---
+++ src/minecraft/net/machinemuse/powersuits/powermodule/movement/AttributeModifier.scala
@@ -0,0 +1,40 @@
+package net.machinemuse.powersuits.powermodule.movement
+
+import net.minecraft.nbt.NBTTagCompound
+
+/**
+ * Author: MachineMuse (Claire Semple)
+ * Created: 10:10 AM, 8/7/13
+ */
+case class AttributeModifier(operation: Int, uuid: UUID, amount: Double, attributeName: String, name: String) {
+  def this(nbt: NBTTagCompound) = this(
+    nbt.getInteger("Operation"),
+    new UUID(nbt),
+    nbt.getDouble("Amount"),
+    nbt.getString("AttributeName"),
+    nbt.getString("Name")
+  )
+
+  def toNBT:NBTTagCompound = toNBT(new NBTTagCompound)
+
+  def toNBT(nbt: NBTTagCompound) = {
+    nbt.setInteger("Operation", operation)
+    uuid.toNBT(nbt)
+    nbt.setDouble("Amount", amount)
+    nbt.setString("AttributeName", attributeName)
+    nbt.setString("Name", name)
+    nbt
+  }
+}
+
+case class UUID(least:Long,most:Long) {
+  def this(nbt: NBTTagCompound) = this(
+    nbt.getLong("UUIDLeast"),
+    nbt.getLong("UUIDMost")
+  )
+
+  def toNBT(nbt: NBTTagCompound) = {
+    nbt.setLong("UUIDLeast", least)
+    nbt.setLong("UUIDMost", most)
+  }
+}
---
+++ src/minecraft/net/machinemuse/powersuits/powermodule/movement/SprintAssistModule.scala
@@ -0,0 +1,94 @@
+package net.machinemuse.powersuits.powermodule.movement
+
+import net.machinemuse.api.IModularItem
+import net.machinemuse.api.ModuleManager
+import net.machinemuse.api.moduletrigger.IPlayerTickModule
+import net.machinemuse.api.moduletrigger.IToggleableModule
+import net.machinemuse.powersuits.item.ItemComponent
+import net.machinemuse.powersuits.powermodule.PowerModuleBase
+import net.machinemuse.utils.ElectricItemUtils
+import net.machinemuse.utils.MuseCommonStrings
+import net.machinemuse.utils.MuseItemUtils
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.nbt.NBTTagList
+import net.minecraft.util.StatCollector
+import java.util.List
+import net.machinemuse.general.MuseLogger
+
+object SprintAssistModule {
+  val MODULE_SPRINT_ASSIST: String = "Sprint Assist"
+  val SPRINT_ENERGY_CONSUMPTION: String = "Sprint Energy Consumption"
+  val SPRINT_SPEED_MULTIPLIER: String = "Sprint Speed Multiplier"
+  val SPRINT_FOOD_COMPENSATION: String = "Sprint Exhaustion Compensation"
+  val WALKING_ENERGY_CONSUMPTION: String = "Walking Energy Consumption"
+  val WALKING_SPEED_MULTIPLIER: String = "Walking Speed Multiplier"
+  val TAGUUID: UUID = UUID(-7931854408382894632l, -8160638015224787553l)
+}
+
+class SprintAssistModule(validItems: List[IModularItem]) extends PowerModuleBase(validItems) with IToggleableModule with IPlayerTickModule {
+
+  import SprintAssistModule._
+
+  addInstallCost(MuseItemUtils.copyAndResize(ItemComponent.servoMotor, 4))
+  addSimpleTradeoff(this, "Power", SPRINT_ENERGY_CONSUMPTION, "J", 0, 10, SPRINT_SPEED_MULTIPLIER, "%", 1, 2)
+  addSimpleTradeoff(this, "Compensation", SPRINT_ENERGY_CONSUMPTION, "J", 0, 2, SPRINT_FOOD_COMPENSATION, "%", 0, 1)
+  addSimpleTradeoff(this, "Walking Assist", WALKING_ENERGY_CONSUMPTION, "J", 0, 10, WALKING_SPEED_MULTIPLIER, "%", 1, 1)
+
+  def getCategory: String = MuseCommonStrings.CATEGORY_MOVEMENT
+
+  def getDataName: String = MODULE_SPRINT_ASSIST
+
+  def getLocalizedName: String = StatCollector.translateToLocal("module.sprintAssist.name")
+
+  def getDescription: String = "A set of servo motors to help you sprint (double-tap forward) and walk faster."
+
+  def onPlayerTickActive(player: EntityPlayer, item: ItemStack) {
+    val horzMovement: Double = Math.sqrt(player.motionX * player.motionX + player.motionZ * player.motionZ)
+    val totalEnergy: Double = ElectricItemUtils.getPlayerEnergy(player)
+    if (player.isSprinting) {
+      val exhaustion: Double = Math.round(horzMovement * 100.0F) * 0.01
+      val sprintCost: Double = ModuleManager.computeModularProperty(item, SPRINT_ENERGY_CONSUMPTION)
+      if (sprintCost < totalEnergy) {
+        val sprintMultiplier: Double = ModuleManager.computeModularProperty(item, SPRINT_SPEED_MULTIPLIER)
+        val exhaustionComp: Double = ModuleManager.computeModularProperty(item, SPRINT_FOOD_COMPENSATION)
+        ElectricItemUtils.drainPlayerEnergy(player, sprintCost * horzMovement * 5)
+        setMovementModifier(item, sprintMultiplier)
+        player.getFoodStats.addExhaustion((-0.01 * exhaustion * exhaustionComp).asInstanceOf[Float])
+        player.jumpMovementFactor = player.getAIMoveSpeed * .5f
+      }
+    }
+    else {
+      val cost: Double = ModuleManager.computeModularProperty(item, WALKING_ENERGY_CONSUMPTION)
+      if (cost < totalEnergy) {
+        val walkMultiplier: Double = ModuleManager.computeModularProperty(item, WALKING_SPEED_MULTIPLIER)
+        ElectricItemUtils.drainPlayerEnergy(player, cost * horzMovement * 5)
+        setMovementModifier(item, walkMultiplier)
+        player.jumpMovementFactor = player.getAIMoveSpeed * .5f
+      }
+    }
+  }
+
+  def setMovementModifier(item: ItemStack, multiplier: Double) {
+    val modifiers: NBTTagList = item.getTagCompound.getTagList("AttributeModifiers")
+    item.getTagCompound.setTag("AttributeModifiers", modifiers)
+    val sprintModifiers =
+      for (i <- 0 until modifiers.tagCount()) yield {
+        val tag = modifiers.tagAt(i).asInstanceOf[NBTTagCompound]
+        if (new AttributeModifier(tag).attributeName == "Sprint Assist") {
+          Some(tag)
+        } else None
+      } flatMap {
+        tag: NBTTagCompound =>
+          tag.setDouble("Amount", multiplier)
+          Some(tag)
+      }
+    if (sprintModifiers.isEmpty) modifiers.appendTag(AttributeModifier(2, TAGUUID, multiplier, "generic.movementSpeed", "Sprint Assist").toNBT)
+  }
+
+  def onPlayerTickInactive(player: EntityPlayer, item: ItemStack) {
+  }
+
+  def getTextureFile: String = "sprintassist"
+}
