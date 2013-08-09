f96770d4d4290f7fd1a35db8d57f715f2b1abbe7
Ben Spiers <ben.spiers22@gmail.com>
2013-08-09 02:35:24 +0100

    Add a way of calling the player energy methods through the IModularItem interface to allow other mods' modules to use them through the ItemStack passed to IRightClickModule methods. Mods will have to convert their own energy though.

--- src/minecraft/net/machinemuse/api/IModularItem.java
+++ src/minecraft/net/machinemuse/api/IModularItem.java
@@ -13,12 +13,36 @@ import net.minecraft.item.ItemStack;
 public interface IModularItem
 {
 
-	/**
-	 * Gets the item's extended summary for displaying in the gui.
-	 * 
-	 * @param stack
-	 * @return
-	 */
-	public List<String> getLongInfo(EntityPlayer player, ItemStack stack);
+    /**
+     * Gets the item's extended summary for displaying in the gui.
+     * 
+     * @param stack
+     * @return
+     */
+    public List<String> getLongInfo(EntityPlayer player, ItemStack stack);
+
+    /**
+     * Returns the amount of energy contained in the player's inventory.
+     * 
+     * @param player
+     * @return
+     */
+    public double getPlayerEnergy(EntityPlayer player);
+
+    /**
+     * Drains the amount of energy from the player's inventory.
+     * 
+     * @param player
+     * @return
+     */
+    public void drainPlayerEnergy(EntityPlayer player, double drainAmount);
+
+    /**
+     * Adds the amount of energy to the player's inventory.
+     * 
+     * @param player
+     * @return
+     */
+    public void givePlayerEnergy(EntityPlayer player, double joulesToGive);
 
 }
