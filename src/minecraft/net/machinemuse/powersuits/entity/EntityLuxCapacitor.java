d2ffe378bbfa3f7aede93906e62994d2663fa5e3
MachineMuse <muse@machinemuse.net>
2013-10-03 11:34:22 -0600

    Lux Capacitors no longer stick to non-solid blocks

--- src/minecraft/net/machinemuse/powersuits/entity/EntityLuxCapacitor.java
+++ src/minecraft/net/machinemuse/powersuits/entity/EntityLuxCapacitor.java
@@ -13,38 +13,37 @@ import net.minecraftforge.common.ForgeDirection;
 
 public class EntityLuxCapacitor extends EntityThrowable {
 
-	public EntityLuxCapacitor(World par1World) {
-		super(par1World);
-	}
+    public EntityLuxCapacitor(World par1World) {
+        super(par1World);
+    }
 
-	public EntityLuxCapacitor(World par1World, EntityLivingBase shootingEntity) {
-		super(par1World, shootingEntity);
-		Vec3 direction = shootingEntity.getLookVec().normalize();
-		double speed = 1.0;
-		this.motionX = direction.xCoord * speed;
-		this.motionY = direction.yCoord * speed;
-		this.motionZ = direction.zCoord * speed;
-		double r = 0.4375;
-		double xoffset = 0.1;
-		double yoffset = 0;
-		double zoffset = 0;
-		double horzScale = Math.sqrt(direction.xCoord * direction.xCoord + direction.zCoord * direction.zCoord);
-		double horzx = direction.xCoord / horzScale;
-		double horzz = direction.zCoord / horzScale;
-		this.posX = shootingEntity.posX + direction.xCoord * xoffset - direction.yCoord * horzx * yoffset - horzz * zoffset;
-		this.posY = shootingEntity.posY + shootingEntity.getEyeHeight() + direction.yCoord * xoffset + (1 - Math.abs(direction.yCoord)) * yoffset;
-		this.posZ = shootingEntity.posZ + direction.zCoord * xoffset - direction.yCoord * horzz * yoffset + horzx * zoffset;
-		this.boundingBox.setBounds(posX - r, posY - 0.0625, posZ - r, posX + r, posY + 0.0625, posZ + r);
-	}
+    public EntityLuxCapacitor(World par1World, EntityLivingBase shootingEntity) {
+        super(par1World, shootingEntity);
+        Vec3 direction = shootingEntity.getLookVec().normalize();
+        double speed = 1.0;
+        this.motionX = direction.xCoord * speed;
+        this.motionY = direction.yCoord * speed;
+        this.motionZ = direction.zCoord * speed;
+        double r = 0.4375;
+        double xoffset = 0.1;
+        double yoffset = 0;
+        double zoffset = 0;
+        double horzScale = Math.sqrt(direction.xCoord * direction.xCoord + direction.zCoord * direction.zCoord);
+        double horzx = direction.xCoord / horzScale;
+        double horzz = direction.zCoord / horzScale;
+        this.posX = shootingEntity.posX + direction.xCoord * xoffset - direction.yCoord * horzx * yoffset - horzz * zoffset;
+        this.posY = shootingEntity.posY + shootingEntity.getEyeHeight() + direction.yCoord * xoffset + (1 - Math.abs(direction.yCoord)) * yoffset;
+        this.posZ = shootingEntity.posZ + direction.zCoord * xoffset - direction.yCoord * horzz * yoffset + horzx * zoffset;
+        this.boundingBox.setBounds(posX - r, posY - 0.0625, posZ - r, posX + r, posY + 0.0625, posZ + r);
+    }
 
-	/**
-	 * Gets the amount of gravity to apply to the thrown entity with each tick.
-	 */
-	@Override
-	protected float getGravityVelocity()
-	{
-		return 0;
-	}
+    /**
+     * Gets the amount of gravity to apply to the thrown entity with each tick.
+     */
+    @Override
+    protected float getGravityVelocity() {
+        return 0;
+    }
 
     @Override
     public void onEntityUpdate() {
@@ -53,22 +52,31 @@ public class EntityLuxCapacitor extends EntityThrowable {
             this.setDead();
         }
     }
-	@Override
-	protected void onImpact(MovingObjectPosition movingobjectposition) {
 
-		if (!this.isDead && movingobjectposition.typeOfHit == EnumMovingObjectType.TILE) {
-			ForgeDirection dir = ForgeDirection.values()[movingobjectposition.sideHit].getOpposite();
-			int x = movingobjectposition.blockX - dir.offsetX;
-			int y = movingobjectposition.blockY - dir.offsetY;
-			int z = movingobjectposition.blockZ - dir.offsetZ;
-			if (y > 0) {
-				int blockID = worldObj.getBlockId(x, y, z);
-				if (blockID <= 0 || Block.blocksList[blockID] == null || Block.blocksList[blockID].isAirBlock(worldObj, x, y, z)) {
-					worldObj.setBlock(x, y, z, BlockLuxCapacitor.assignedBlockID, 0, 7);
-					worldObj.setBlockTileEntity(x, y, z, new TileEntityLuxCapacitor(dir));
-				}
-			}
-			this.setDead();
-		}
-	}
+    @Override
+    protected void onImpact(MovingObjectPosition movingobjectposition) {
+
+        if (!this.isDead && movingobjectposition.typeOfHit == EnumMovingObjectType.TILE) {
+            ForgeDirection dir = ForgeDirection.values()[movingobjectposition.sideHit].getOpposite();
+            int x = movingobjectposition.blockX - dir.offsetX;
+            int y = movingobjectposition.blockY - dir.offsetY;
+            int z = movingobjectposition.blockZ - dir.offsetZ;
+            if (y > 0) {
+                int blockID = worldObj.getBlockId(x, y, z);
+                if (blockID <= 0 || Block.blocksList[blockID] == null || Block.blocksList[blockID].isAirBlock(worldObj, x, y, z)) {
+                    for (ForgeDirection d : ForgeDirection.values()) {
+                        int xo = x + d.offsetX;
+                        int yo = y + d.offsetY;
+                        int zo = z + d.offsetZ;
+                        int blockToStickTo = worldObj.getBlockId(xo, yo, zo);
+                        if(Block.isNormalCube(blockToStickTo)) {
+                            worldObj.setBlock(x, y, z, BlockLuxCapacitor.assignedBlockID, 0, 7);
+                            worldObj.setBlockTileEntity(x, y, z, new TileEntityLuxCapacitor(d));
+                        }
+                    }
+                }
+            }
+            this.setDead();
+        }
+    }
 }
