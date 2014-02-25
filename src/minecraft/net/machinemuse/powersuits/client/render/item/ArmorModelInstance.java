62552c40a6b83214f50380663ea3b9bc174ae266
MachineMuse <claire.alexandria@gmail.com>
2014-02-25 08:45:27 -0700

    added debug messages to smart moving support

--- src/minecraft/net/machinemuse/powersuits/client/render/item/ArmorModelInstance.java
+++ src/minecraft/net/machinemuse/powersuits/client/render/item/ArmorModelInstance.java
@@ -1,5 +1,7 @@
 package net.machinemuse.powersuits.client.render.item;
 
+import net.machinemuse.numina.general.MuseLogger;
+
 /**
  * Author: MachineMuse (Claire Semple)
  * Created: 10:01 PM, 11/07/13
@@ -11,8 +13,11 @@ public class ArmorModelInstance {
         if (instance == null) {
 //            instance = new VanillaArmorModel();
             try {
+                MuseLogger.logDebug("Attempting to load Smart Moving armor model.");
                 instance = new SMovingArmorModel();
+                MuseLogger.logDebug("Smart Moving armor model loaded successfully!");
             } catch (Throwable e) {
+                MuseLogger.logDebug("Smart Moving armor model did not load successfully. Either Smart Moving is not installed, or there was another problem.");
                 instance = new VanillaArmorModel();
             }
         }
