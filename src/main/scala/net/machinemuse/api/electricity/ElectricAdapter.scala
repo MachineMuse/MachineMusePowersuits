649d72111f86d35001703b16bdde9afe3ecdf32b
MachineMuse <claire.alexandria@gmail.com>
2014-09-09 14:55:01 -0600

    added RF support back in (untested)

--- src/main/scala/net/machinemuse/api/electricity/ElectricAdapter.scala
+++ src/main/scala/net/machinemuse/api/electricity/ElectricAdapter.scala
@@ -1,5 +1,7 @@
 package net.machinemuse.api.electricity
 
+import cofh.api.energy.IEnergyContainerItem
+import net.machinemuse.powersuits.common.ModCompatability
 import net.minecraft.item.ItemStack
 
 object ElectricAdapter {
@@ -7,7 +9,7 @@ object ElectricAdapter {
     if (stack == null) return null
     stack.getItem match {
       case i: MuseElectricItem => new MuseElectricAdapter(stack)
-      //      case i: IEnergyContainerItem => if (ModCompatability.isCoFHCoreLoaded) new TEElectricAdapter(stack) else null
+            case i: IEnergyContainerItem => if (ModCompatability.isCoFHCoreLoaded) new TEElectricAdapter(stack) else null
       //      case i: IElectricItem => if (ModCompatability.isIndustrialCraftLoaded) new IC2ElectricAdapter(stack) else null
       case _ => null
     }
@@ -51,16 +53,16 @@ class MuseElectricAdapter(val stack: ItemStack) extends ElectricAdapter {
 //
 //  def getTier = item.getTier(stack)
 //}
-//
-//class TEElectricAdapter(val stack: ItemStack) extends ElectricAdapter {
-//  val item = stack.getItem.asInstanceOf[IEnergyContainerItem]
-//
-//  def getCurrentEnergy: Double = museEnergyFromRF(item.getEnergyStored(stack))
-//
-//  def getMaxEnergy: Double = museEnergyFromRF(item.getMaxEnergyStored(stack))
-//
-//  def drainEnergy(requested: Double): Double = museEnergyFromRF(item.extractEnergy(stack, museEnergyToRF(requested), false))
-//
-//  def giveEnergy(provided: Double): Double = museEnergyFromRF(item.receiveEnergy(stack, museEnergyToRF(provided), false))
-//
-//}
+
+class TEElectricAdapter(val stack: ItemStack) extends ElectricAdapter {
+  val item = stack.getItem.asInstanceOf[IEnergyContainerItem]
+  import ElectricConversions._
+  def getCurrentEnergy: Double = museEnergyFromRF(item.getEnergyStored(stack))
+
+  def getMaxEnergy: Double = museEnergyFromRF(item.getMaxEnergyStored(stack))
+
+  def drainEnergy(requested: Double): Double = museEnergyFromRF(item.extractEnergy(stack, museEnergyToRF(requested), false))
+
+  def giveEnergy(provided: Double): Double = museEnergyFromRF(item.receiveEnergy(stack, museEnergyToRF(provided), false))
+
+}
