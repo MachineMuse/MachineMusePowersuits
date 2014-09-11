6d27408b50caca14b672b106b98a8340c2efef1f
MachineMuse <claire.alexandria@gmail.com>
2014-09-11 08:33:13 -0600

    updated galacticraft breathable helmet

--- src/main/scala/net/machinemuse/powersuits/item/ItemPowerArmorHelmet.scala
+++ src/main/scala/net/machinemuse/powersuits/item/ItemPowerArmorHelmet.scala
@@ -2,33 +2,29 @@ package net.machinemuse.powersuits.item
 
 import cpw.mods.fml.common.Optional
 import cpw.mods.fml.relauncher.{Side, SideOnly}
+import micdoodle8.mods.galacticraft.api.item.IBreathableArmor
+import micdoodle8.mods.galacticraft.api.item.IBreathableArmor.EnumGearType
 import net.machinemuse.api.ModuleManager
-import net.machinemuse.powersuits.powermodule.misc.ThaumGogglesModule
+import net.machinemuse.powersuits.powermodule.misc.{AirtightSealModule, ThaumGogglesModule}
 import net.machinemuse.utils.render.MuseRenderer
 import net.minecraft.client.renderer.texture.IIconRegister
 import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
 import thaumcraft.api.IGoggles
 import thaumcraft.api.nodes.IRevealer
 
 @Optional.Interface(iface = "thaumcraft.api.IGoggles", modid = "Thaumcraft", striprefs = true)
 @Optional.Interface(iface = "thaumcraft.api.nodes.IRevealer", modid = "Thaumcraft", striprefs = true)
+@Optional.Interface(iface = "micdoodle8.mods.galacticraft.api.item.IBreathableArmor", modid = "GalacticraftCore", striprefs = true)
 class ItemPowerArmorHelmet extends ItemPowerArmor(0, 0)
-//with IBreathableArmor
+with IBreathableArmor
 with IGoggles
 with IRevealer
 {
   val iconpath = MuseRenderer.ICON_PREFIX + "armorhead"
 
   setUnlocalizedName("powerArmorHelmet")
-
-  //
-  //  // IBreathableArmor
-  //  def handleGearType(gearType: EnumGearType): Boolean = gearType eq EnumGearType.HELMET
-  //
-  //  def canBreathe(helm: ItemStack, player: EntityPlayer, gearType: EnumGearType): Boolean =
-  //    ModuleManager.itemHasActiveModule(helm, AirtightSealModule.AIRTIGHT_SEAL_MODULE)
-
 
   @SideOnly(Side.CLIENT)
   override def registerIcons(iconRegister: IIconRegister) {
@@ -40,4 +36,13 @@ with IRevealer
 
   override def showNodes(itemstack: ItemStack, player: EntityLivingBase): Boolean =
     ModuleManager.itemHasActiveModule(itemstack, ThaumGogglesModule.MODULE_THAUM_GOGGLES)
+
+  @Optional.Method(modid = "GalacticraftCore")
+  override def handleGearType(geartype: EnumGearType): Boolean = {
+    geartype == EnumGearType.HELMET
+  }
+
+  @Optional.Method(modid = "GalacticraftCore")
+  override def canBreathe(helm: ItemStack, player: EntityPlayer, geartype: EnumGearType): Boolean =
+    ModuleManager.itemHasActiveModule(helm, AirtightSealModule.AIRTIGHT_SEAL_MODULE)
 }
