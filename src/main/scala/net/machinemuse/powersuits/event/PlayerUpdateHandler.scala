440e3048a60e1326d97263d6758bbbb26bb02ed3
Korynkai <matt@qmxtech.com>
2015-01-24 02:58:48 +0000

    Oh geez...

--- src/main/scala/net/machinemuse/powersuits/event/PlayerUpdateHandler.scala
+++ src/main/scala/net/machinemuse/powersuits/event/PlayerUpdateHandler.scala
@@ -38,6 +38,7 @@ class PlayerUpdateHandler {
             }
           }
         }
+        import scala.collection.JavaConversions._
         if (!moduleActive) {
             module.onPlayerTickInactive(player, itemStack)
         }
