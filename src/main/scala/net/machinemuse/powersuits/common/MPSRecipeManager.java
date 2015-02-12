002ae982795b6df4946e1b3b2e6aa6ef518c6c8e
Korynkai <matt@qmxtech.com>
2015-02-12 03:37:27 +0000

    Umm, yeah, we need that path here...

--- src/main/scala/net/machinemuse/powersuits/common/MPSRecipeManager.java
+++ src/main/scala/net/machinemuse/powersuits/common/MPSRecipeManager.java
@@ -16,11 +16,11 @@ public class MPSRecipeManager {
     public static void loadOrPutRecipesFromJar(String path) {
     	if (!isLoaded) {
     		try {
-    		    String folder = "/assets/powersuits/recipes";
+    		    String recipeJarPath = "/assets/powersuits/recipes";
                 if (ModCompatability.vanillaRecipesEnabled()) {
                     File vanilla = new File(path, "vanilla.recipes");
                     if (!vanilla.isFile()) {
-                        FileUtils.copyURLToFile(MPSRecipeManager.class.getResource("/vanilla.recipes"), vanilla);
+                        FileUtils.copyURLToFile(MPSRecipeManager.class.getResource(recipeJarPath + "/vanilla.recipes"), vanilla);
                     }
                     JSONRecipeList.loadRecipesFromFile(vanilla);
                 }
@@ -29,7 +29,7 @@ public class MPSRecipeManager {
                 //     if (ModCompatability.UERecipesEnabled()) {
                 //         File ue = new File(path, "UniversalElectricity.recipes");
                 //         if (!ue.isFile()) {
-                //             FileUtils.copyURLToFile(MPSRecipeManager.class.getResource("/UniversalElectricity.recipes"), ue);
+                //             FileUtils.copyURLToFile(MPSRecipeManager.class.getResource(recipeJarPath + "/UniversalElectricity.recipes"), ue);
                 //         }
                 //         JSONRecipeList.loadRecipesFromFile(ue);
                 //     }
@@ -39,7 +39,7 @@ public class MPSRecipeManager {
                 //     if (ModCompatability.EnderIORecipesEnabled()) {
                 //         File eio = new File(path, "EnderIO.recipes");
                 //         if (!eio.isFile()) {
-                //             FileUtils.copyURLToFile(MPSRecipeManager.class.getResource("/EnderIO.recipes"), eio);
+                //             FileUtils.copyURLToFile(MPSRecipeManager.class.getResource(recipeJarPath + "/EnderIO.recipes"), eio);
                 //         }
                 //         JSONRecipeList.loadRecipesFromFile(eio);
                 //     }
@@ -49,7 +49,7 @@ public class MPSRecipeManager {
                 //     if (ModCompatability.MekanismRecipesEnabled()) {
                 //         File mk = new File(path, "Mekanism.recipes");
                 //         if (!mk.isFile()) {
-                //             FileUtils.copyURLToFile(MPSRecipeManager.class.getResource("/Mekanism.recipes"), mk);
+                //             FileUtils.copyURLToFile(MPSRecipeManager.class.getResource(recipeJarPath + "/Mekanism.recipes"), mk);
                 //         }
                 //         JSONRecipeList.loadRecipesFromFile(mk);
                 //     }
@@ -58,7 +58,7 @@ public class MPSRecipeManager {
     				if (ModCompatability.IC2RecipesEnabled()) {
 	        		    File ic2 = new File(path, "IndustrialCraft2.recipes");
 	            		if (!ic2.isFile()) {
-	                		FileUtils.copyURLToFile(MPSRecipeManager.class.getResource("/IndustrialCraft2.recipes"), ic2);
+	                		FileUtils.copyURLToFile(MPSRecipeManager.class.getResource(recipeJarPath + "/IndustrialCraft2.recipes"), ic2);
 	            		}
 	            		JSONRecipeList.loadRecipesFromFile(ic2);
     				}
@@ -66,7 +66,7 @@ public class MPSRecipeManager {
 	        		    if (ModCompatability.GregTechRecipesEnabled()) {
 		                    File gt = new File(path, "GregTech.recipes");
 		            		if (!gt.isFile()) {
-		                		FileUtils.copyURLToFile(MPSRecipeManager.class.getResource("/GregTech.recipes"), gt);
+		                		FileUtils.copyURLToFile(MPSRecipeManager.class.getResource(recipeJarPath + "/GregTech.recipes"), gt);
 		            		}
 		            		JSONRecipeList.loadRecipesFromFile(gt);
 	        		    }
@@ -76,7 +76,7 @@ public class MPSRecipeManager {
                     if (ModCompatability.ThermalExpansionRecipesEnabled()) {
                         File te = new File(path, "ThermalExpansion.recipes");
                         if (!te.isFile()) {
-                            FileUtils.copyURLToFile(MPSRecipeManager.class.getResource("/ThermalExpansion.recipes"), te);
+                            FileUtils.copyURLToFile(MPSRecipeManager.class.getResource(folder + "/ThermalExpansion.recipes"), te);
                         }
                         JSONRecipeList.loadRecipesFromFile(te);
                     }
