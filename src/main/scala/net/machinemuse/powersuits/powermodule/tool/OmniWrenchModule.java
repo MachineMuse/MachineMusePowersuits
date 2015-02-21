e403cfcc3640ebf6f933b856c5b184e0f7d7bf93
Korynkai <matt@qmxtech.com>
2015-02-21 15:47:22 +0000

    Oh geez... Forgot to remove the EnderIO methods from OmniWrenchModule.

--- src/main/scala/net/machinemuse/powersuits/powermodule/tool/OmniWrenchModule.java
+++ src/main/scala/net/machinemuse/powersuits/powermodule/tool/OmniWrenchModule.java
@@ -107,18 +107,18 @@ public boolean onItemUseFirst(ItemStack itemStack, EntityPlayer player, World wo
 
         TileEntity tile = world.getTileEntity(x, y, z);
 
-        if (ModCompatability.isEnderIOLoaded()) {
-                // Hmm... Seems this allows breakage of more EnderIO blocks than the YetaWrench...
-                // Though it seems there's no better way to provide similar functionality with EnderIO machines... - 2014-12-01 Korynkai
-                if (tile instanceof TileEntityEio) {
-                        if (player.isSneaking()) {
-                                b.removedByPlayer(world, player, x, y, z, true);
-                        } else {
-                                b.rotateBlock(world, x, y, z, ForgeDirection.getOrientation(side));
-                        }
-                }
-                return !world.isRemote;
-        }
+        // if (ModCompatability.isEnderIOLoaded()) {
+        //         // Hmm... Seems this allows breakage of more EnderIO blocks than the YetaWrench...
+        //         // Though it seems there's no better way to provide similar functionality with EnderIO machines... - 2014-12-01 Korynkai
+        //         if (tile instanceof TileEntityEio) {
+        //                 if (player.isSneaking()) {
+        //                         b.removedByPlayer(world, player, x, y, z, true);
+        //                 } else {
+        //                         b.rotateBlock(world, x, y, z, ForgeDirection.getOrientation(side));
+        //                 }
+        //         }
+        //         return !world.isRemote;
+        // }
         // IC2: UNTESTED - 2014-12-01 Korynkai
         if (ModCompatability.isIndustrialCraftLoaded()) {
                 if (tile instanceof IWrenchable) {
