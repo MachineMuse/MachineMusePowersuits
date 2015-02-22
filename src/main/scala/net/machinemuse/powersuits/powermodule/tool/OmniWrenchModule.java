9c27cebba3a704874cb808524b439ce0fc9cdf4d
Korynkai <matt@qmxtech.com>
2015-02-22 22:26:19 +0000

    ...

--- src/main/scala/net/machinemuse/powersuits/powermodule/tool/OmniWrenchModule.java
+++ src/main/scala/net/machinemuse/powersuits/powermodule/tool/OmniWrenchModule.java
@@ -193,21 +193,22 @@ public void onPlayerStoppedUsing(ItemStack itemStack, World world, EntityPlayer 
 }
 
 @Override
-public void onPlayerTickActive(EntityPlayer player, ItemStack item) {
+public void onPlayerTickActive(EntityPlayer player, ItemStack stack) {
         if (ModCompatability.isEnderIOLoaded()) {
+								ItemStack item = player.getHeldItem();
                 if (item != null && item.getItem() instanceof IModularItem) {
                         if (!MuseItemTag.getMuseItemTag(item).getBoolean("eioFacadeTransparency")) {
                                 MuseItemTag.getMuseItemTag(item).setString("eioNoCompete", MODULE_OMNI_WRENCH);
                                 MuseItemTag.getMuseItemTag(item).setBoolean("eioFacadeTransparency", true);
-                                MuseItemTag.getMuseItemTag(item).setBoolean("eioManipulateConduit", true);
                         }
                 }
         }
 }
 
 @Override
-public void onPlayerTickInactive(EntityPlayer player, ItemStack item) {
+public void onPlayerTickInactive(EntityPlayer player, ItemStack stack) {
         if (ModCompatability.isEnderIOLoaded()) {
+								ItemStack item = player.getHeldItem();
                 if (item != null && item.getItem() instanceof IModularItem) {
                         if ((MuseItemTag.getMuseItemTag(item).getString("eioNoCompete") != null) 
                         		&& (!MuseItemTag.getMuseItemTag(item).getString("eioNoCompete").isEmpty())) {
@@ -215,13 +216,11 @@ public void onPlayerTickInactive(EntityPlayer player, ItemStack item) {
                                         MuseItemTag.getMuseItemTag(item).setString("eioNoCompete", "");
                                         if (MuseItemTag.getMuseItemTag(item).getBoolean("eioFacadeTransparency")) {
                                                 MuseItemTag.getMuseItemTag(item).setBoolean("eioFacadeTransparency", false);
-                                                MuseItemTag.getMuseItemTag(item).setBoolean("eioManipulateConduit", false);
                                         }
                                 }
                         } else {
                                 if (MuseItemTag.getMuseItemTag(item).getBoolean("eioFacadeTransparency")) {
                                         MuseItemTag.getMuseItemTag(item).setBoolean("eioFacadeTransparency", false);
-                                        MuseItemTag.getMuseItemTag(item).setBoolean("eioManipulateConduit", false);
                                 }
                         }
                 }
