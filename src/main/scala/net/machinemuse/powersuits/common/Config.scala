67dd9c470aa4c501c8a083450097438d2b8f068f
Korynkai <matt@qmxtech.com>
2015-02-24 21:58:54 +0000

    Final tweaks for 0.11.0.7-QMX. Also fix README.md in gitignore...

--- src/main/scala/net/machinemuse/powersuits/common/Config.scala
+++ src/main/scala/net/machinemuse/powersuits/common/Config.scala
@@ -145,7 +145,7 @@ object Config {
     addModule(new WaterElectrolyzerModule(HEADONLY))
     addModule(new NightVisionModule(HEADONLY))
     addModule(new FlightControlModule(HEADONLY))
-    // addModule(new InvisibilityModule(TORSOONLY)) Removed until 0.11.0.7-QMX - See GitHub issue #15
+    // addModule(new InvisibilityModule(TORSOONLY)) Removed until 0.11.0.8-QMX - See GitHub issue #15
     addModule(new BlinkDriveModule(TOOLONLY))
     addModule(new DiamondPickUpgradeModule(TOOLONLY))
     addModule(new AquaAffinityModule(TOOLONLY))
