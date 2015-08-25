c0855df9894abc993eccc3b32e37169fc23d3f4a
MachineMuse <muse@machinemuse.net>
2015-08-25 06:20:56 -0600

    Added maxdamage=0 so hopefully things stop breaking the armor unnecessarily. Closes #579

---
+++ src/main/scala/net/machinemuse/api/IModularItem.scala
@@ -0,0 +1,52 @@
+package net.machinemuse.api
+
+import java.util.List
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
+
+/**
+ * Interface for ItemPowerArmor and ItemPowerTool to share.
+ *
+ * @author MachineMuse
+ */
+trait IModularItem extends net.minecraft.item.Item {
+  /**
+   * Gets the item's extended summary for displaying in the gui.
+   *
+   * @param stack
+   * @return
+   */
+  def getLongInfo(player: EntityPlayer, stack: ItemStack): List[String]
+
+  /**
+   * Returns the amount of energy contained in the player's inventory.
+   *
+   * @param player
+   * @return
+   */
+  def getPlayerEnergy(player: EntityPlayer): Double
+
+  /**
+   * Drains the amount of energy from the player's inventory.
+   *
+   * @param player
+   * @return
+   */
+  def drainPlayerEnergy(player: EntityPlayer, drainAmount: Double)
+
+  /**
+   * Adds the amount of energy to the player's inventory.
+   *
+   * @param player
+   * @return
+   */
+  def givePlayerEnergy(player: EntityPlayer, joulesToGive: Double)
+
+
+  /** Since this is the only mixin shared between the two, it's simplest to set it here.
+    *
+    * @param itemStack
+    * @return 0
+    */
+  override def getMaxDamage(itemStack:ItemStack) = 0
+}
