f5db0cc0e3e83cfc207c2f14b2495235cb4f4f83
MachineMuse <muse@machinemuse.net>
2015-08-28 03:44:03 -0600

    Boots now count as metal armor for IC2 magnetic transport

--- src/main/scala/net/machinemuse/powersuits/item/ItemPowerArmorBoots.scala
+++ src/main/scala/net/machinemuse/powersuits/item/ItemPowerArmorBoots.scala
@@ -1,11 +1,18 @@
 package net.machinemuse.powersuits.item
 
+import cpw.mods.fml.common.Optional
+import ic2.api.item.IMetalArmor
 import net.minecraft.client.renderer.texture.IIconRegister
 import cpw.mods.fml.relauncher.Side
 import cpw.mods.fml.relauncher.SideOnly
 import net.machinemuse.utils.render.MuseRenderer
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
 
-class ItemPowerArmorBoots extends ItemPowerArmor(0, 3) {
+@Optional.InterfaceList (Array(
+  new Optional.Interface (iface = "ic2.api.item.IMetalArmor", modid = "IC2", striprefs = true)
+) )
+class ItemPowerArmorBoots extends ItemPowerArmor(0, 3) with IMetalArmor{
   val iconpath = MuseRenderer.ICON_PREFIX + "armorfeet"
 
   setUnlocalizedName("powerArmorBoots")
@@ -13,4 +20,6 @@ class ItemPowerArmorBoots extends ItemPowerArmor(0, 3) {
   @SideOnly(Side.CLIENT) override def registerIcons(iconRegister: IIconRegister) {
     itemIcon = iconRegister.registerIcon(iconpath)
   }
+
+  override def isMetalArmor(itemStack: ItemStack, entityPlayer: EntityPlayer): Boolean = true
 }
