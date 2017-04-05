0ca6c761ac90be37c676a1f9eed761a5602a40e9
lehjr <lehjr1@gmail.com>
2017-04-04 23:18:33 -0400

    Powerfist models mostly fixed now.

--- src/main/java/net/machinemuse/powersuits/client/render/item/ToolModel.java
+++ src/main/java/net/machinemuse/powersuits/client/render/item/ToolModel.java
@@ -365,17 +365,14 @@ public class ToolModel extends ModelBase {
         palm.addChild(thumb1);
     }
 
-    public static int xtap = 0;
-    public static int ytap = 0;
-    public static int ztap = 0;
-    public static float xOffest = 0;
-    public static float yOffest = 0;
-    public static float zOffest = 0;
-    public static float scalemodifier = 1;
-
-
-
-    public static boolean tap;
+//    public static int xtap = 0;
+//    public static int ytap = 0;
+//    public static int ztap = 0;
+//    public static float xOffest = 0;
+//    public static float yOffest = 0;
+//    public static float zOffest = 0;
+//    public static float scalemodifier = 1;
+//    public static boolean tap;
 
     public void makeChild(ModelRenderer child, ModelRenderer parent) {
         parent.addChild(child);
@@ -388,124 +385,114 @@ public class ToolModel extends ModelBase {
     }
 
 
-    /*
-     * Only used for setting up scale, rotation, and relative placement coordinates
-     */
-    private void transformCalibration() {
-//        super.render(entity, f, f1, f2, f3, f4, f5);
-        int numsegments = 16;
-        if (!tap) {
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_INSERT)) {
-                xOffest += 0.1;
-                tap = true;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_DELETE)) {
-                xOffest -= 0.1;
-                tap = true;
-            }
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_HOME)) {
-                yOffest += 0.1;
-                tap = true;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_END)) {
-                yOffest -= 0.1;
-                tap = true;
-            }
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_PRIOR)) {
-                zOffest += 0.1;
-                tap = true;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_NEXT)) {
-                zOffest -= 0.1;
-                tap = true;
-            }
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD1)) {
-                xtap += 1;
-                tap = true;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD2)) {
-                ytap += 1;
-                tap = true;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD3)) {
-                ztap += 1;
-                tap = true;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD4)) {
-                xtap -= 1;
-                tap = true;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD5)) {
-                ytap -= 1;
-                tap = true;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD6)) {
-                ztap -= 1;
-                tap = true;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD8)) {
-                xtap = 0;
-                ytap = 0;
-                ztap = 0;
-
-                xOffest = 0;
-                yOffest = 0;
-                zOffest = 0;
-
-                scalemodifier = 1;
-
-                tap = true;
-            }
-
-//            if (Keyboard.isKeyDown(Keyboard.KEY_SCROLL)) {
-//                scalemodifier -= 1;
-//                tap = true;
-//            }
-//            if (Keyboard.isKeyDown(Keyboard.KEY_PAUSE)) {
-//                scalemodifier += 1;
-//                tap = true;
-//            }
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD0)) {
-                System.out.println("xrot: " + xtap + ", yrot: " + ytap + ", zrot: " + ztap);
-
-                System.out.println("xOffest: " + xOffest + ", yOffest: " + yOffest + ", zOffest: " + zOffest);
-
-                System.out.println("scaleModifier: " + scalemodifier);
-
-                tap = true;
+//    /*
+//     * Only used for setting up scale, rotation, and relative placement coordinates
+//     */
+//    private void transformCalibration() {
+//        int numsegments = 16;
+//        if (!tap) {
 //
-//            [20:24:23] [Client thread/INFO]: [net.machinemuse.powersuits.client.render.item.ToolModel:render:459]: xrot: -178, yrot: 0, zrot: 0
-//                    [20:24:23] [Client thread/INFO]: [net.machinemuse.powersuits.client.render.item.ToolModel:render:461]: xrot: -3.0999992, yrot: 1.5000001, zrot: 9.299999
-            }
-        }
-        else {
-            if (!Keyboard.isKeyDown(Keyboard.KEY_NUMPAD0) && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD1) && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD2)
-                    && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD3) && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD4)
-                    && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD5) && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD6)) {
-                tap = false;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
-                tap = false;
-            }
-        }
-    }
-
-
-
+//            if (Keyboard.isKeyDown(Keyboard.KEY_INSERT)) {
+//                xOffest += 0.1;
+//                tap = true;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_DELETE)) {
+//                xOffest -= 0.1;
+//                tap = true;
+//            }
+//
+//            if (Keyboard.isKeyDown(Keyboard.KEY_HOME)) {
+//                yOffest += 0.1;
+//                tap = true;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_END)) {
+//                yOffest -= 0.1;
+//                tap = true;
+//            }
+//
+//            if (Keyboard.isKeyDown(Keyboard.KEY_PRIOR)) {
+//                zOffest += 0.1;
+//                tap = true;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_NEXT)) {
+//                zOffest -= 0.1;
+//                tap = true;
+//            }
+//
+//            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD1)) {
+//                xtap += 1;
+//                tap = true;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD2)) {
+//                ytap += 1;
+//                tap = true;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD3)) {
+//                ztap += 1;
+//                tap = true;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD4)) {
+//                xtap -= 1;
+//                tap = true;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD5)) {
+//                ytap -= 1;
+//                tap = true;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD6)) {
+//                ztap -= 1;
+//                tap = true;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD8)) {
+//                xtap = 0;
+//                ytap = 0;
+//                ztap = 0;
+//
+//                xOffest = 0;
+//                yOffest = 0;
+//                zOffest = 0;
+//
+//                scalemodifier = 1;
+//
+//                tap = true;
+//            }
+//              // this probably needs a bit more work, int's are too big.
+////            if (Keyboard.isKeyDown(Keyboard.KEY_SCROLL)) {
+////                scalemodifier -= 1;
+////                tap = true;
+////            }
+////            if (Keyboard.isKeyDown(Keyboard.KEY_PAUSE)) {
+////                scalemodifier += 1;
+////                tap = true;
+////            }
+//
+//            if (Keyboard.isKeyDown(Keyboard.KEY_NUMPAD0)) {
+//                System.out.println("xrot: " + xtap + ", yrot: " + ytap + ", zrot: " + ztap);
+//
+//                System.out.println("xOffest: " + xOffest + ", yOffest: " + yOffest + ", zOffest: " + zOffest);
+//
+//                System.out.println("scaleModifier: " + scalemodifier);
+//
+//                tap = true;
+//            }
+//        }
+//        else {
+//            if (!Keyboard.isKeyDown(Keyboard.KEY_NUMPAD0) && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD1) && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD2)
+//                    && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD3) && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD4)
+//                    && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD5) && !Keyboard.isKeyDown(Keyboard.KEY_NUMPAD6)) {
+//                tap = false;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+//                tap = false;
+//            }
+//        }
+//    }
 
     public void render(Entity entity, float scale, ItemCameraTransforms.TransformType cameraTransformTypeIn, Colour c1, Colour glow) {
-//
-
         GL11.glPushMatrix();
         GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
         GL11.glDisable(GL11.GL_CULL_FACE);
 //        GL11.glEnable(GL11.GL_DEPTH_TEST);
-
         MuseTextureUtils.pushTexture(Config.SEBK_TOOL_TEXTURE);
 
         if (c1 != null) {
@@ -513,23 +500,16 @@ public class ToolModel extends ModelBase {
         }
         double scale1 = 1.0 / 16.0; // 0.0625
 
-        transformCalibration();
-
-
-
-//[22:29:51] [Client thread/INFO]: [net.machinemuse.powersuits.client.render.item.ToolModel:render:475]: xrot: -182, yrot: 0, zrot: 0
-//[22:29:51] [Client thread/INFO]: [net.machinemuse.powersuits.client.render.item.ToolModel:render:477]: xOffest: -2.9999993, yOffest: 2.2, zOffest: 8.999998
-// xOffest: -3.1999993, yOffest: -1.4000001, zOffest: -8.999998
+//        transformCalibration();// only needed for setting up the model
 
         if (cameraTransformTypeIn == FIRST_PERSON_RIGHT_HAND || cameraTransformTypeIn == FIRST_PERSON_LEFT_HAND) {
-            GL11.glScaled(-scale1, scale1, scale1);
+            GL11.glScaled(-scale1, scale1, scale1); // negative scale mirrors the model
+            GL11.glRotatef(-182, 1, 0, 0);
 
-            GL11.glTranslatef(xOffest, yOffest, zOffest);
-            GL11.glRotatef(xtap, 1, 0, 0);
-            GL11.glRotatef(ytap, 0, 1, 0);
-            GL11.glRotatef(ztap, 0, 0, 1);
-
-//            GL11.glRotatef(-182, 1, 0, 0);
+//            GL11.glTranslatef(xOffest, yOffest, zOffest);
+//            GL11.glRotatef(xtap, 1, 0, 0);
+//            GL11.glRotatef(ytap, 0, 1, 0);
+//            GL11.glRotatef(ztap, 0, 0, 1);
 
         } else {
             GL11.glScaled(scale1, scale1, scale1);
@@ -542,9 +522,7 @@ public class ToolModel extends ModelBase {
 //            GL11.glRotatef(ztap, 0, 0, 1);
         }
 
-
         GL11.glPushMatrix();
-
         if (cameraTransformTypeIn == FIRST_PERSON_RIGHT_HAND || cameraTransformTypeIn == FIRST_PERSON_LEFT_HAND) {
             mainarm.render(scale);
         }
