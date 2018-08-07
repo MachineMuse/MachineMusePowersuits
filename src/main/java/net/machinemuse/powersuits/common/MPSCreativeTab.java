11143aa71e12e98b592236ae5648358f0f31c20c
lehjr <lehjr1@gmail.com>
2018-08-07 11:46:41 -0400

    finished IC2 recipes

--- src/main/java/net/machinemuse/powersuits/common/MPSCreativeTab.java
+++ src/main/java/net/machinemuse/powersuits/common/MPSCreativeTab.java
@@ -2,15 +2,18 @@ package net.machinemuse.powersuits.common;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Ported to Java by lehjr on 11/3/16.
  */
 public class MPSCreativeTab extends CreativeTabs {
     public MPSCreativeTab() {
-        super(CreativeTabs.getNextID(), "powersuits");
+        super("powersuits");
     }
 
+    @SideOnly(Side.CLIENT)
     @Override
     public ItemStack getTabIconItem() {
         return new ItemStack(MPSItems.powerArmorHead);
