a384f37a099a68e50cd98436fc8a888bd1e8ac2d
lehjr <lehjr1@gmail.com>
2018-09-24 22:10:42 -0400

    tweaked recipes and loading to mimic how it worked in 1.10.x

--- src/main/java/net/machinemuse/powersuits/recipe/MPSRecipeConditionFactory.java
+++ src/main/java/net/machinemuse/powersuits/recipe/MPSRecipeConditionFactory.java
@@ -17,29 +17,33 @@ public class MPSRecipeConditionFactory implements IConditionFactory {
         if(JsonUtils.hasField(json, "type")) {
             String key = JsonUtils.getString(json, "type");
 
+            // Thermal Expansion
+            if (key.equals("powersuits:thermal_expansion_recipes_enabled"))
+                return () -> ModCompatibility.isThermalExpansionLoaded() == value;
+
             // EnderIO
             if (key.equals("powersuits:enderio_recipes_enabled"))
                 return () -> ModCompatibility.isEnderIOLoaded() == value;
 
-            // GregTech
-            if (key.equals("powersuits:gregtech_recipes_enabled"))
-                return () -> ModCompatibility.isGregTechLoaded() == value;
-
-            // IC2
-            if (key.equals("powersuits:ic2_recipes_enabled"))
-                return () -> ModCompatibility.isIndustrialCraftExpLoaded() == value;
-
-            // IC2 Classic
-            if (key.equals("powersuits:ic2_classic_recipes_enabled"))
-                return () -> ModCompatibility.isIndustrialCraftExpLoaded() == value;
-
+            // Original recipe loading code set priority for TechReborn recipes instead of Gregtech or Industrialcraft
             // Tech Reborn
             if (key.equals("powersuits:tech_reborn_recipes_enabled"))
                 return () -> ModCompatibility.isTechRebornLoaded() == value;
 
-            // Thermal Expansion
-            if (key.equals("powersuits:thermal_expansion_recipes_enabled"))
-                return () -> ModCompatibility.isThermalExpansionLoaded() == value;
+            // GregTech
+            if (key.equals("powersuits:gregtech_recipes_enabled"))
+                return () -> (ModCompatibility.isGregTechLoaded() && !ModCompatibility.isTechRebornLoaded()) == value;
+
+            // IC2
+            if (key.equals("powersuits:ic2_recipes_enabled"))
+                return () -> (ModCompatibility.isIndustrialCraftExpLoaded() &&
+                        (!ModCompatibility.isGregTechLoaded() && !ModCompatibility.isTechRebornLoaded())) == value;
+
+            // IC2 Classic
+            if (key.equals("powersuits:ic2_classic_recipes_enabled"))
+                return () -> (ModCompatibility.isIndustrialCraftClassicLoaded() &&
+                        (!ModCompatibility.isGregTechLoaded() && !ModCompatibility.isTechRebornLoaded())) == value;
+
 
             // Vanilla reciples only as fallback
             if (key.equals("powersuits:vanilla_recipes_enabled")) {
